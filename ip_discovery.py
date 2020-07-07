b27ca8583d1635417fd3a48b60cc8121de6022d9
crashwind <crw.inbox@gmail.com>
2020-07-07 10:25:04 +0300

    added: hostname list support

--- ip_discovery.py
+++ ip_discovery.py
@@ -7,22 +7,28 @@ import click
 import json
 
 
-def hosts_discovery(hostname):
+def discover(hostnames=[]):
     """
-    :hostname:  dns hostname
+    :hostnames: dns hostnames list
     :return:    zabbix discovery format
+    :example:   {"data": [{"{#HOSTNAME}": "www.google.ru", "{#IP}": "108.177.14.94"}]}
     """
 
-    ips = socket.gethostbyname_ex(hostname)[2]
-    result = {'data': [{'{#IP}': x} for x in ips]}
+    assert isinstance(hostnames, list), 'hostnames must be list'
 
+    data = []
+    for hostname in hostnames:
+        ips = socket.gethostbyname_ex(hostname)[2]
+        data.extend([{'{#HOSTNAME}': hostname, '{#IP}': x} for x in ips])
+
+    result = {'data': data}
     logging.debug(result)
     return json.dumps(result)
 
 
 @click.command()
 @click.option("--log-level", '-l', help='Log level', default='info')
-@click.option('--hostname', '-s', help='hostname to check', default='www.gismeteo.ru')
+@click.option('--hostname', '-s', help='hostnames to check seperated by comma', default='www.gismeteo.ru')
 def main(hostname, log_level):
 
     # setup logging
@@ -32,7 +38,8 @@ def main(hostname, log_level):
     except Exception as e:
         print('ERROR: {}'.format(e))
 
-    print(hosts_discovery(hostname))
+    hostnames = hostname.split(',')
+    print(discover(hostnames))
 
 
 if __name__ == '__main__':
